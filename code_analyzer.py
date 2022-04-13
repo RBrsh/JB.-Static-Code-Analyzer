c3f8a9c657a9fe6b9c6e86f467b05aa3646ac250
JB <y.v.barash@gmail.com>
2022-04-13 14:11:12 +0200

    Stage 2/5. First edits. Initial Class structure.

--- code_analyzer.py
+++ code_analyzer.py
@@ -1,10 +1,49 @@
-def main():
-    path_to_file = input()
+class CodeAnalyzer:
+    def __init__(self, path_to_file):
+        self.path_to_file = path_to_file
+        self.__errors = []
+        self.__checks = []
+        self.__prepare_checks()
 
-    with open(path_to_file) as fh:
-        for ln, l in enumerate(fh.readlines(), 1):
-            if len(l) > 79:
-                print(f'Line {ln}: S001 Too long')
+    def perform_checks(self):
+        with open(self.path_to_file) as fh:
+            for ln, l in enumerate(fh.readlines(), 1):
+                for m in self.__checks:
+                    m(l)
+
+    def check_line_length(self, line: str):
+        print('line')
+
+    def check_indentation(self, line: str):
+        print('indentation')
+
+    def check_semicolon(self, line: str):
+        print('semi')
+
+    def check_spaces(self, line: str):
+        print('spaces')
+
+    def check_todo(self, line: str):
+        print('TODO')
+
+    def check_blank_lines(self, line: str):
+        print('Blank lines')
+
+    def __prepare_checks(self):
+        for mn in self.__dir__():
+            if mn.startswith('check_'):
+                m = getattr(self, mn)
+                if callable(m):
+                    self.__checks.append(m)
+
+    def get_errors(self):
+        return self.__errors
+
+
+def main():
+    code_analyzed = CodeAnalyzer(input())
+    code_analyzed.perform_checks()
+    print(code_analyzed.get_errors())
 
 
 if __name__ == '__main__':
